515106f78d25b18cf28a26cd7d337cb824947d5f
Kos-Mos <ChuyM203@gmail.com>
2020-09-15 20:21:35 -0400

    Added user-route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 
 const router = require("express").Router();
 const { Post, User } = require("../../models/");
-const withAuth = require("../../auth");
+const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
     Post.findAll({
