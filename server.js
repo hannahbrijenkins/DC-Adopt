9b70ccb29ea22bf228c0b6e5f8b2474f9817c20a
Junioresc <junioresc1092@gmail.com>
2020-09-15 21:13:07 -0400

    included the routes inside of their respective index files

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const path = require("path");
 const express = require("express");
+const routes = require(`./controllers`);
+const sequelize = require(`./config/connection`);
+const path = require("path");
+
 // const exphbs = require("express-handlebars");
 
 const app = express();
@@ -11,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
