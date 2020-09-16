0327e981a8e01a7fae8586f33208e916e946e4ed
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-09-16 00:42:55 -0400

    fixed login handlebar and server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine(`handlebars`, exphbs);
+app.engine(`handlebars`, exphbs({ defaultLayout: 'main' }));
 app.set(`view engine`, `handlebars`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
