1bec8e068e3bf45f89f6a7bdf06716365c640baf
Junioresc <junioresc1092@gmail.com>
2020-09-16 20:02:49 -0400

    fixed the signup form not sending info to the database

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ async function signupFormHandler(event) {
     const password = document.querySelector('#password').value.trim();
 
     if(username && email && password) {
-        const response = await fetch('/api/user', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
                 username,
@@ -25,4 +25,4 @@ async function signupFormHandler(event) {
     }
 }
 
-document.querySelector('#signupBtn').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
